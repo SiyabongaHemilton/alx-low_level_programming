5b132862e6077af41695a12aae9eb55e90474dee
SiyabongaHemilton <cyabongakingz@gmail.com>
2023-06-22 02:13:08 -0700

    Like,comment,subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,12 +2,6 @@
 
 /**
  * main - Entry point of the program
- *
- * Description: This program demonstrates an infinite loop.
- *              It prints a message indicating an infinite loop is coming,
- *              then initializes a variable i to 0, and finally prints
- *              a message indicating the infinite loop has been avoided.
- *
  * Return: Always 0 (Success)
  */
 int main(void)
