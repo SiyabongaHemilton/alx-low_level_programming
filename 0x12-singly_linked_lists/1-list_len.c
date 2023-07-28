89c013b7e8cff5147387f3e278cb4e2b27682765
SiyabongaHemilton <cyabongakingz@gmail.com>
2023-07-28 01:40:33 -0700

    issue with description for list_len fixed

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,7 +2,7 @@
 #include "lists.h"
 
 /**
- * list len - returns the number of elements in a linked list
+ * list_len - function returns the number of elements in a linked list
  * @h: pointer to the list_t list
  *
  * Return: number of elements in h
